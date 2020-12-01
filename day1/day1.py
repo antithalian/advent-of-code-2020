bdc1fedee68894844e861cbf13e93cb94d9232f8
John Sullivan <jpsullivan2019@gmail.com>
2020-11-30 23:20:07 -0800

    day one finished

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,26 @@
 # day one solution
 # find product of two numbers in input.txt list that sum to 2020
 
-with open("input.txt", "r") as file:
-    pass
+# pull everything out of input file
+nums = []
+with open('input.txt', 'r') as file:
+    for line in file:
+        nums.append(int(line))
+
+target = 0
+product = 0
+
+# for each number in the input, figure out what it's complement to 2020 would be
+for ini in nums:
+
+    target = 2020 - ini
+
+    # then iterate through and check if its complement exists
+    for chk in nums:
+
+        # if it does, compute the product
+        # semi-hacky since it assumes there'll only be one pair
+        if chk == target:
+            product = ini * chk
+
+print(product)
