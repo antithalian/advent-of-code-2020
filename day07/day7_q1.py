c7a8ae47cdd9a0ad02e297733a9f40bda3ebff7c
John Sullivan <jpsullivan2019@gmail.com>
2020-12-08 00:26:17 -0800

    adds day 7 and 8 ideas - will finish later

---
+++ day07/day7_q1.py
@@ -0,0 +1,4 @@
+# day 7 question 1
+# how many base bag colors can eventually hold a shiny gold bag?
+
+# construct dictionary of what's possible, recursively check???
---
+++ day08/day8_q1.py
@@ -0,0 +1,4 @@
+# day 8 question 1
+# determine when an instruction will be executed twice
+
+# put everything into an array, maintain a dict/hashmap of what's been executed?
