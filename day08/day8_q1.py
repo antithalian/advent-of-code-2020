e5384b08ae34b7df6b8f67ef3e2b324785d5583b
John Sullivan <jpsullivan2019@gmail.com>
2020-12-09 00:25:43 -0800

    adds day 8 q1 solution

--- day08/day8_q1.py
+++ day08/day8_q1.py
@@ -2,3 +2,33 @@
 # determine when an instruction will be executed twice
 
 # put everything into an array, maintain a dict/hashmap of what's been executed?
+
+# process input
+instructions = []
+with open('input.txt', 'r') as file:
+
+    for line in file:
+        line = line.strip().split(' ')
+        instr = (line[0], int(line[1]))
+        instructions.append(instr)
+
+visited = {}
+ptr = 0
+acc = 0
+found = False
+while not found:
+
+    curr = instructions[ptr]
+    if str(ptr) in visited.keys():
+        found = True
+    else:
+        visited[str(ptr)] = 1
+        if curr[0] == 'nop':
+            ptr += 1
+        elif curr[0] == 'acc':
+            acc += curr[1]
+            ptr += 1
+        else:
+            ptr += curr[1]
+
+print(acc)
