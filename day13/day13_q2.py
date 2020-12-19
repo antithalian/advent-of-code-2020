d98898a8280d84056345fe61150bfcc1ac9e1761
John Sullivan <jpsullivan2019@gmail.com>
2020-12-18 22:39:48 -0800

    adds day 13 challenge 2

--- day13/day13_q2.py
+++ day13/day13_q2.py
@@ -1,12 +1,21 @@
 # day 13 challenge 2
-# shuttle contest
+# matching times
 
-# get input
-tstamp, step = 0, 1
-ids = []
+# read input
 with open('input.txt', mode='r') as file:
-    tstamp = int(file.readline())
-    for id in file.readline().strip().split(','):
-        if id != 'x':
-            ids.append(int(id))
+    pairs = [(int(id), offset) for offset, id in enumerate(file.read().strip().split()[1].split(',')) if id != 'x']
+    # build a set of tuples of bus id and offset from start
 
+# tracking vars
+t, step = 0, 1
+
+# iterate pairs
+for id, offset in pairs:
+
+    # is bus departing?
+    while (t + offset) % id != 0:
+        t += step
+    # check next bus
+    step *= id
+
+print(t)
